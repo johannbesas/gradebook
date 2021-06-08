d03e6b5138b96d66510bf67d71c325e9fe94e3a1
Johann Besas <johann.besas@iag.co.nz>
2021-06-08 16:05:43 +1200

    update unit tests

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -6,14 +6,13 @@ namespace GradeBook.Tests
     public class BookTests
     {
         [Fact]
-        public void Test1()
+        public void BookCalculatesAnAverageGrade()
         {
             // arrange
             var book = new Book("");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.3);
-
 
             // act
             var result = book.GetStatistics();
