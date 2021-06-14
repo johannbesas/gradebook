afa67dd397a4d79f4ea7e12bf8bf361b9bffd3d7
Johann Besas <johann.besas@iag.co.nz>
2021-06-14 15:30:05 +1200

    add switch

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -11,6 +11,31 @@ namespace GradeBook
             this.Name = name;
         }
 
+        public void AddLetterGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(65);
+                    break;
+                case 'F':
+                    AddGrade(50);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
         public void AddGrade(double grade)
         {
             if (grade <= 100 && grade >= 0)
